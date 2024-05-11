a567c93f303a27985e1159b2a46859017be815da
Athailah <tuxkeren@gmail.com>
2024-05-11 23:56:15 +0700

    form

--- P6/product_new.php
+++ P6/product_new.php
@@ -34,7 +34,14 @@
 </nav>
 
 <div class="container mt-5">
-        
+    <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
 </div>
 
 <div class="mt-5 p-4 bg-dark text-white text-center">
